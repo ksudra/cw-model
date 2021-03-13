5744aa54a59e2a7b2662f241bc59df5c7506e2f1
eo20203 <78903047+eo20203@users.noreply.github.com>
2021-03-13 12:22:27 +0000

    Update MyGameStateFactory.java

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -17,132 +17,93 @@ import java.util.stream.Collectors;
  * cw-model
  * Stage 1: Complete this class
  */
+
 public final class MyGameStateFactory implements Factory<GameState> {
+
 
 	@Nonnull @Override public GameState build(
 			GameSetup setup,
 			Player mrX,
 			ImmutableList<Player> detectives) {
-		// TODO
-		throw new RuntimeException("Implement me!");
-
+		return new MyGameState(setup, ImmutableSet.of(Piece.MrX.MRX), ImmutableList.of(), mrX, detectives);
 	}
 
 //	private class that implements methods of the GameState interface.
-	private final class MyGameState implements GameState {
-		@Nonnull
-		@Override
-		public GameSetup getSetup() {
-			return null;
+		private final class MyGameState implements GameState {
+			private GameSetup setup;
+			private ImmutableSet<Piece> remaining;
+			private ImmutableList<LogEntry> log;
+			private Player mrX;
+			private List<Player> detectives;
+			private ImmutableList<Player> everyone;
+			private ImmutableSet<Move> moves;
+			private ImmutableSet<Piece> winner;
+
+		private MyGameState(
+				final GameSetup setup,
+				final ImmutableSet<Piece> remaining,
+				final ImmutableList<LogEntry> log,
+				final Player mrX,
+				final List<Player> detectives) {
+			this.setup = setup;
+			if(this.setup.graph == null) throw new IllegalArgumentException("Graph is empty");
+			if(this.setup.rounds.isEmpty()) throw new IllegalArgumentException("Rounds is empty");
+			this.remaining = remaining;
+			if(this.remaining.isEmpty()) throw new IllegalArgumentException("Pieces is empty");
+			this.log = log;
+			this.mrX = mrX;
+			if(this.mrX == null) throw new NullPointerException("There is no Mr.X");
+			this.detectives = detectives;
+			if(this.detectives.isEmpty()) throw new IllegalArgumentException("There are no detectives");
+			for(int i = 0; i < detectives.size(); i++) {
+				if(detectives.get(i).piece() == null) throw new NullPointerException("Detective is null");
+			}
 		}
 
-		@Nonnull
-		@Override
-		public ImmutableSet<Piece> getPlayers() {
-			return null;
-		}
-
-		@Nonnull
-		@Override
-		public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
-			return Optional.empty();
-		}
-
-		@Nonnull
-		@Override
-		public Optional<TicketBoard> getPlayerTickets(Piece piece) {
-			return Optional.empty();
-		}
-
-		@Nonnull
-		@Override
-		public ImmutableList<LogEntry> getMrXTravelLog() {
-			return null;
-		}
-
-		@Nonnull
-		@Override
-		public ImmutableSet<Piece> getWinner() {
-			return null;
-		}
-
-		@Nonnull
-		@Override
-		public ImmutableSet<Move> getAvailableMoves() {
-			return null;
-		}
-
-		@Nonnull
-		@Override
-		public GameState advance(Move move) {
-			return null;
-		}
-
-		//Method to read plain text graph, based on the method from worksheet 3
-		ImmutableValueGraph<Integer, ScotlandYard.Transport> readGraph(String content) {
-			List<String> lines = content.lines().collect(Collectors.toList());
-			if (lines.isEmpty()) throw new IllegalArgumentException("No lines");
-			int currentLine = 0;
-
-			String[] topLine = lines.get(currentLine++).split(" ");
-			int numberOfNodes = Integer.parseInt(topLine[0]);
-			int numberOfEdges = Integer.parseInt(topLine[1]);
-
-			ImmutableValueGraph.Builder<Integer, ScotlandYard.Transport> builder = ValueGraphBuilder
-					.undirected()
-					.expectedNodeCount(numberOfNodes)
-					.immutable();
-
-
-			for (int i = 0; i < numberOfNodes; i++) {
-				String line = lines.get(currentLine++);
-				if (line.isEmpty()) continue;
-				builder.addNode(Integer.parseInt(line));
+			@Override
+			public GameSetup getSetup() {
+				return setup;
 			}
 
-//			Create a list of arrays to store all edges as undirected
-			List<int[]> list = new ArrayList<>();
-			int[] a= new int[2];
-
-//			Adds all nodes to list of undirected edges.
-			for (int i = 0; i < numberOfEdges; i++) {
-				String line = lines.get(currentLine++);
-				if (line.isEmpty()) continue;
-
-				String[] s = line.split(" ");
-				a[0] = Integer.parseInt(s[0]);
-				a[1] = Integer.parseInt(s[1]);
-				list.add(a);
+			@Override
+			public ImmutableSet<Piece> getPlayers() {
+				return null;
 			}
 
-			for (int i = 0; i < numberOfEdges; i++) {
-				String line = lines.get(currentLine++);
-				if (line.isEmpty()) continue;
-
-				String[] s = line.split(" ");
-				ScotlandYard.Transport transport = null;
-				a[0] = Integer.parseInt(s[0]);
-				a[1] = Integer.parseInt(s[1]);
-				list.add(a);
-
-//				Determines the type of transport based on the plain text file
-				if(s[2] == "Taxi") {
-					transport = ScotlandYard.Transport.TAXI;
-				} else  if (s[2] == "Bus") {
-					transport = ScotlandYard.Transport.BUS;
-				} else  if (s[2] == "Underground") {
-					transport = ScotlandYard.Transport.UNDERGROUND;
-				} else {
-					transport = ScotlandYard.Transport.FERRY;
+			@Override
+			public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
+				int loc = 0;
+				for(int i = 0; i < detectives.size(); i++) {
+					if(detectives.get(i).piece() == detective) {
+						loc = detectives.get(i).location();
+					}
 				}
-
-//				Adds edge to graph if the line contains to nodes and an edge value.
-				if (s.length != 3) throw new IllegalArgumentException("Bad edge line:" + line);
-				builder.putEdgeValue(Integer.parseInt(s[0]),
-						Integer.parseInt(s[1]),
-						transport);
+				return Optional.of(Optional.of(loc)).orElse(Optional.empty());
 			}
-			return builder.build();
-		}
+
+			@Override
+			public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+				return Optional.empty();
+			}
+
+			@Override
+			public ImmutableList<LogEntry> getMrXTravelLog() {
+				return log;
+			}
+
+			@Override
+			public ImmutableSet<Piece> getWinner() {
+				return null;
+			}
+
+			@Override
+			public ImmutableSet<Move> getAvailableMoves() {
+				return null;
+			}
+
+			@Override
+			public GameState advance(Move move) {
+				return null;
+			}
 	}
 }
