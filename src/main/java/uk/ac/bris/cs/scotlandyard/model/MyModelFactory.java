140c0ac5035ca4523d185d0d2ea3622db17d67ea
eo20203 <78903047+eo20203@users.noreply.github.com>
2021-03-25 11:02:16 +0000

    model structure

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
@@ -7,12 +7,16 @@ import javax.annotation.Nonnull;
 import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
 
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
 /**
  * cw-model
  * Stage 2: Complete this class
  */
 public final class MyModelFactory  implements Factory<Model> {
-	public ScotlandYard.Factory<Board.GameState> gameStateFactory;
+	MyGameStateFactory gameStateFactory = new MyGameStateFactory();
 
 	@Nonnull @Override public Model build(GameSetup setup,
 	                                      Player mrX,
@@ -34,17 +38,23 @@ public final class MyModelFactory  implements Factory<Model> {
 		@Nonnull
 		@Override
 		public Board getCurrentBoard() {
-			return null;
+			return new MyBoard(state);
 		}
 
 		@Override
 		public void registerObserver(@Nonnull Observer observer) {
-
+			Set<Observer> newObservers = new HashSet<>();
+			newObservers.addAll(observers);
+			newObservers.add(observer);
+			this.observers = ImmutableSet.copyOf(newObservers);
 		}
 
 		@Override
 		public void unregisterObserver(@Nonnull Observer observer) {
-
+			Set<Observer> newObservers = new HashSet<>();
+			newObservers.addAll(observers);
+			newObservers.remove(observer);
+			this.observers = ImmutableSet.copyOf(newObservers);
 		}
 
 		@Nonnull
@@ -56,6 +66,55 @@ public final class MyModelFactory  implements Factory<Model> {
 		@Override
 		public void chooseMove(@Nonnull Move move) {
 
+		}
+
+		final class MyBoard implements Board {
+			private GameState gameState;
+			private MyBoard(GameState gameState) {
+				this.gameState = gameState;
+			}
+
+			@Nonnull
+			@Override
+			public GameSetup getSetup() {
+				return gameState.getSetup();
+			}
+
+			@Nonnull
+			@Override
+			public ImmutableSet<Piece> getPlayers() {
+				return gameState.getPlayers();
+			}
+
+			@Nonnull
+			@Override
+			public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
+				return gameState.getDetectiveLocation(detective);
+			}
+
+			@Nonnull
+			@Override
+			public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+				return gameState.getPlayerTickets(piece);
+			}
+
+			@Nonnull
+			@Override
+			public ImmutableList<LogEntry> getMrXTravelLog() {
+				return gameState.getMrXTravelLog();
+			}
+
+			@Nonnull
+			@Override
+			public ImmutableSet<Piece> getWinner() {
+				return gameState.getWinner();
+			}
+
+			@Nonnull
+			@Override
+			public ImmutableSet<Move> getAvailableMoves() {
+				return gameState.getAvailableMoves();
+			}
 		}
 	}
 }
